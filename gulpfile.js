d738dcd57d62abf156ec77879abef0abf0f50431
w4rlock <warlock.gpl@gmail.com>
2014-04-13 01:37:10 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp')
+  , connect = require('gulp-connect')
+  , pathreload = ['./public/*.html'];
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'public/',
+    port: 8082,
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src(pathreload[0])
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch([pathreload[0]], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express')
+  , jwt = require('jsonwebtoken')
+  , auth = require('express-jwt')
+  , compress = require('compression')()
+  , http = require('http')
+  , validator = require('express-validator')
+  , cron = require('cron').CronJob
+  , path = require('path')
+  , app = express()
+  , config = require('./config').init(app)
+  , db = require('monk')(config.DB_URL)
+  , mail = require('./lib/mail');
+
+mail.init(config);
+
+var secret = "4$4bmQH23+$IFTRMv34R5seffeceE0EmC8YQ4o$";
+
+//app.use(express.logger('dev'));
+//app.use(express.methodOverride());
+app.use(compress);
+app.use('/api', auth({secret: secret }));
+app.use(require('body-parser')())
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(validator());
+//app.use(express.responseTime());
+//app.use(express.compress());
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//-- ROUTES
+app.get('/', function(req, res) { res.send(app.routes); });
+app.all('*', function(req, res){ res.send({ err: 'Route not found'}, 404);});
+
+http.createServer(app).listen(config.APP_PORT, 
+  function() {
+    console.log("\n[*] Server Listening on port %d", config.APP_PORT); 
+  }
+);
+
+//Seconds: 0-59, Minutes: 0-59, Hours: 0-23, Day of Month: 1-31, Months: 0-11, Day of Week: 0-6
+//new cron('30 02 21 * * 5', 
+  //function(){
+    //console.log('[*] Cron job started');
+    //console.log('[*] Actualizando Aseguradoras (WebService)');
+    //ars.updateAseguradoras(ws,db);
+  //}, 
+//null, true);
