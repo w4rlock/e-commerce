789246fb3ffd8fa4731c1f47e92bae2e8b039ca1
w4rlock <warlock.gpl@gmail.com>
2014-04-15 00:05:57 -0300

    correcciones mejoras

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp')
   , connect = require('gulp-connect')
-  , pathreload = ['./public/*'];
+  , clean = require('gulp-clean');
+
+var paths = {
+    html: ['**/*.html']
+  , reloadAll: ['!./public/bower_co*/**', './public/**/{*.html,*.css,*.js}']
+  , images: 'client/img/**/*'
+};
 
 gulp.task('connect', function() {
   connect.server({
@@ -10,13 +16,20 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('html', function () {
-  gulp.src(pathreload[0])
-    .pipe(connect.reload());
+gulp.task('reloadAll', function () {
+  gulp.src(paths.reloadAll).pipe(connect.reload());
+});
+
+gulp.task('scripts', function () {
+  gulp.src(paths.js[0]).pipe(connect.reload());
+});
+
+gulp.task('clean', function () {
+   gulp.src('tmp', {read: false}).pipe(clean());
 });
 
 gulp.task('watch', function () {
-  gulp.watch([pathreload[0]], ['html']);
+  gulp.watch(paths.reloadAll, ['reloadAll']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch' ]);
