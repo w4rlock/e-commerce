63c79e97fcb6a98e0f13549901fd033a62ff4865
w4rlock <warlock.gpl@gmail.com>
2014-04-15 14:50:23 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp')
   , connect = require('gulp-connect')
-  , clean = require('gulp-clean');
+  , ngmin = require('gulp-ngmin')
+  , rename = require( 'gulp-rename' )
+  , minifycss = require( 'gulp-minify-css' )
+  , notify = require( 'gulp-notify' )
+  , uglify = require('gulp-uglify');
 
 var paths = {
     html: ['**/*.html']
@@ -20,6 +24,32 @@ gulp.task('reloadAll', function () {
   gulp.src(paths.reloadAll).pipe(connect.reload());
 });
 
+
+gulp.task('minify', function() {
+  gulp.src('./public/scripts/**/*.js')
+    .pipe(ngmin())
+    .pipe(gulp.dest('./public/scripts/'))
+    .pipe(notify({ message: 'Minify js task complete' }));
+
+  gulp.src('./public/css/*.css')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public/css/'))
+    .pipe( notify({ message: 'Minify css task complete' }));
+});
+
+gulp.task('compress', function() {
+  gulp.src('./public/scripts/**/*.js')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('./public/build/'))
+  //});
+
+  //gulp.src('public/scripts/app.js')
+    //.pipe(ngmin())
+    //.pipe(gulp.dest('public/assets/'))
+});
+
 gulp.task('scripts', function () {
   gulp.src(paths.js[0]).pipe(connect.reload());
 });
@@ -32,4 +62,4 @@ gulp.task('watch', function () {
   gulp.watch(paths.reloadAll, ['reloadAll']);
 });
 
-gulp.task('default', ['connect', 'watch' ]);
+gulp.task('default', ['connect', 'minify', 'compress', 'watch' ]);
